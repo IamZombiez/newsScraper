ceca4ca81c9df821b2d3c65d552b05a8951e0cb1
IamZombiez <dereksiu216@gmail.com>
2017-06-01 00:20:07 -0700

    Server Setup

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+// Dependencies
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+// Requiring our Models
+
+// Our scraping tools
+var request = require("request");
+var cheerio = require("cheerio");
+// Set mongoose to leverage built in JavaScript ES6 Promises
+mongoose.Promise = Promise;
+
 //Express
 var express = require('express');
 var app = express();
@@ -6,10 +18,11 @@ var PORT = process.env.PORT || 8080;
 // See files in public folder
 app.use(express.static(__dirname + '/views'));
 
-// Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+// Use morgan and body parser with our app
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
 //For Handlebars
 app.set('views', './views/layouts')
@@ -17,3 +30,17 @@ app.engine('hbs', exphbs({
     extname: '.hbs'
 }));
 app.set('view engine', '.hbs');
+
+// Database configuration with mongoose
+mongoose.connect("#");
+var db = mongoose.connection;
+
+// Show any mongoose errors
+db.on("error", function(error) {
+  console.log("Mongoose Error: ", error);
+});
+
+// Once logged in to the db through mongoose, log a success message
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
