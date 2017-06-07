4489191182eab12fcc7e673be6d6c36f6989dd4a
IamZombiez <dereksiu216@gmail.com>
2017-06-07 11:03:03 -0700

    need to save articles

--- routes/routes.js
+++ routes/routes.js
@@ -1,40 +1,13 @@
-// Dependencies
-var express = require("express");
-var bodyParser = require("body-parser");
-var logger = require("morgan");
-var mongoose = require("mongoose");
-// Requiring our Note and Article models
-var Note = require("./models/notes.js");
-var Article = require("./models/articles.js");
-// Our scraping tools
-var request = require("request");
-var cheerio = require("cheerio");
-// Set mongoose to leverage built in JavaScript ES6 Promises
-mongoose.Promise = Promise;
+// Node Dependencies
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var request = require('request'); // for web-scraping
+var cheerio = require('cheerio'); // for web-scraping
 
-// Initialize Express
-var app = express();
-
-// Morgan and body parser with our app
-app.use(logger("dev"));
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
-
-app.use(express.static("public"));
-
-// DB Setup
-mongoose.connect("mongodb://localhost/news");
-var db = mongoose.connection;
-
-// Err Check
-db.on("error", function(error) {
-  console.log("Mongoose Error: ", error);
-});
-
-db.once("open", function() {
-  console.log("Mongoose connection successful.");
-});
+// Import the Comment and Article models
+var Note = require('../models/notes.js');
+var Article = require('../models/articles.js');
 
 // Routes
 
@@ -206,9 +179,4 @@ app.post("/articles/:id", function(req, res) {
       });
     }
   });
-});
-
-// Listen on port 3000
-app.listen(3000, function() {
-  console.log("App running on port 3000!");
 });
